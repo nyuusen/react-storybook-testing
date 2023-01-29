0afc33dfd2b3e408ebda03d733417c2dc80d1ac8
nyuusen <pppike.0131@gmail.com>
2023-01-30 08:12:49 +0900

    cypress.configにbaseUrlを定義

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,6 @@
 describe('tic tac toe', () => {
   it('x player win', () => {
-    cy.visit('http://localhost:3000')
+    cy.visit('/')
     cy.get('[data-cy="square-0"]').click()
     cy.get('[data-cy="square-1"]').click()
     cy.get('[data-cy="square-3"]').click()
@@ -10,7 +10,7 @@ describe('tic tac toe', () => {
     cy.get('[data-cy="game-status"]').should('have.text', 'Winner: X')
   })
   it('draw', () => {
-    cy.visit('http://localhost:3000')
+    cy.visit('/')
     cy.get('[data-cy="square-0"]').click()
     cy.get('[data-cy="square-4"]').click()
     cy.get('[data-cy="square-3"]').click()
